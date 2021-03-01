2362fa6f601554d0b71c84c739658e7e66156776
nogoodcode <jonfreynik@gmail.com>
2021-03-01 09:34:12 -0500

    building

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-var w = window;
-w.hydra = w.hydra || {};
-require("./hy-lang")(w.hydra);
+'use strict'
+
+export var HydraLang = {
+    isArray: function (arg) {
+        return Object.prototype.toString.call(arg) === '[object Array]';
+    },
+
+    getType: function (arg) {
+        if (arg == null) { 
+            return arg.toString().toLowerCase(); 
+        } 
+
+        var type = Object.prototype.toString.call(arg).slice(8,-1).toLowerCase();
+    
+        if (type === 'generatorfunction') { 
+            return 'function' 
+        }
+
+        if (type.match(/^(array|bigint|date|error|function|generator|regexp|symbol)$/)) {
+            return type;
+        }
+
+        return (typeof arg === 'object' || typeof arg === 'function') ? 'object' : typeof arg;
+
+    },
+
+    serialize: function (arg) {
+
+    },
+
+    unserialize: function (arg) {
+
+    },
+
+    param: function (arg) {
+
+    },
+
+    unparam: function (arg) {
+
+    },
+
+    $_GET: function (arg, source) {
+
+    },
+
+};
