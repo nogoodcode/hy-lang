a32c9849a2fe9f4c06ab9db9de7cb1047f224a20
NoGoodCode <80694033+jgf-7thleveltech@users.noreply.github.com>
2021-03-24 15:29:59 -0400

    buiding

--- src/index.js
+++ src/index.js
@@ -13,6 +13,16 @@ ns.isObject = function (arg)
     return ns.getType(arg) === "object";
 };
 
+ns.isNumber = function (arg) 
+{
+    return typeof arg === "number";
+};
+
+ns.isDate = function (arg)
+{
+    return arg instanceof Date;
+};
+
 ns.getType = function (arg)
 {
     if (arg == null) { 
@@ -42,11 +52,97 @@ ns.getType = function (arg)
 
 ns.bytes = function (arg) 
 {
-
+    // TODO -- ...
+    return 0;
 };
 
-ns.serialize = function (arg) 
+ns.toJSON = function (arg)
 {
+    // TODO - add polyfill
+    return JSON.stringify(arg);
+};
+
+ns.fromJSON = function (arg) 
+{
+    // TODO - add polyfill
+    return JSON.parse(arg);
+};
+
+ns.formatDate = function (arg)
+{
+    if (ns.isDate(arg)) {
+        return arg.getFullYear() + "-" +
+            ("0" + arg.getMonth()).slice(-2) + "-" +
+            ("0" + arg.getDate()).slice(-2) + " " + 
+            
+    } else if (ns.isString(arg)) {
+
+    }
+};
+
+ns.serialize = function (arg, level) 
+{
+    var type = ns.getType(arg), temp, result;
+
+    level = ns.isNumber(level) ? level : 0;
+    switch (type) {
+        case "string":
+        case "number":
+        case "boolean":
+        case "undefined":
+        case "function":
+            result = {
+                t: type.charAt(0),
+                v: arg.toString()
+            };
+            break;
+            
+        case "object":
+            if (angular.isDate($value)){
+                // Date
+                result = {
+                    type: "d",
+                    value: $filter("date")($value, "yyyy-MM-dd HH:mm:ss")
+                };
+            } 
+            
+            else if (angular.isArray($value)) {
+                // Array
+                temp = [];
+                for (var i = 0; i < $value.length; i++) {
+                    temp.push(serialize($value[i], $level + 1));
+                }
+                result = {
+                    type: "a",
+                    value: temp
+                };
+            }
+            
+            else if (
+                value instanceof Element ||
+                value instanceof HTMLElement ||
+                value instanceof HTMLDocument
+            ) {
+                // HTML Element
+                console.warn ("TODO - serialize html elements");
+            } 
+            
+            else {
+                // POJO
+                temp = {};
+                for (var key in $value) {
+                    temp[key] = serialize($value[key], $level + 1);
+                }
+                result = {
+                    type: "o",
+                    value: temp
+                };
+            }
+            break;
+        }
+
+        // only json encode at the end - to save characters
+        return (!$level) ? angular.toJson(result) : result;
 
 };
 
@@ -67,6 +163,97 @@ ns.unparam = function (arg)
 {
 
 };
+
+ns.base64Encode = function (arg)
+{
+    var chars = {
+        ascii: function () {
+            return 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+        },
+        indices: function () {
+            if (!this.cache) {
+                this.cache = {};
+                var ascii = chars.ascii();
+
+                for (var c = 0; c < ascii.length; c++) {
+                    var chr = ascii[c];
+                    this.cache[chr] = c;
+                }
+            }
+            return this.cache;
+        }
+    };
+
+    var ascii = chars.ascii(),
+        len = data.length - 1,
+        i = -1,
+        b64 = '';
+
+    while (i < len) {
+        var code = data.charCodeAt(++i) << 16 | 
+            data.charCodeAt(++i) << 8 | 
+            data.charCodeAt(++i);
+        b64 += ascii[(code >>> 18) & 63] + 
+            ascii[(code >>> 12) & 63] + 
+            ascii[(code >>> 6) & 63] + 
+            ascii[code & 63];
+    }
+
+    var pads = data.length % 3;
+
+    if (pads > 0) {
+        b64 = b64.slice(0, pads - 3);
+        while (b64.length % 4 !== 0) {
+            b64 += '=';
+        }
+    }
+
+    return b64;
+};
+
+ns.base64Decode = function (arg)
+{
+    var chars = {
+        ascii: function () {
+            return 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+        },
+        indices: function () {
+            if (!this.cache) {
+                this.cache = {};
+                var ascii = chars.ascii();
+
+                for (var c = 0; c < ascii.length; c++) {
+                    var chr = ascii[c];
+                    this.cache[chr] = c;
+                }
+            }
+            return this.cache;
+        }
+    };
+
+    var indices = chars.indices(),
+        pos = b64.indexOf('='),
+        padded = pos > -1,
+        len = padded ? pos : b64.length,
+        i = -1,
+        data = '';
+
+    while (i < len) {
+        var code = indices[b64[++i]] << 18 | 
+            indices[b64[++i]] << 12 | 
+            indices[b64[++i]] << 6 | 
+            indices[b64[++i]];
+        if (code !== 0) {
+            data += String.fromCharCode((code >>> 16) & 255, (code >>> 8) & 255, code & 255);
+        }
+    }
+
+    if (padded) {
+        data = data.slice(0, pos - b64.length);
+    }
+
+    return data;
+}
 
 
 export { ns as default }
